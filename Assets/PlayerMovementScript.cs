df7d2970371bd956aa73e25d394026d49ca94896
Garett Cooper <garettcooper@ymail.com>
2017-04-21 21:57:44 -0400

    Development started, B-Cell moving freely around 2D space

---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+	public float speed;
+
+	private float verticalInput;
+	private float horizontalInput;
+	private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		direction = new Vector3();
+	}
+
+	void FixedUpdate() {
+		
+		
+	}
+
+	// Odd unresponsiveness to key release, look for source
+	void Update () {
+
+		verticalInput = Input.GetAxis("Vertical");
+		horizontalInput = Input.GetAxis("Horizontal");
+
+		direction.Set(horizontalInput * Time.deltaTime, verticalInput * Time.deltaTime);
+		direction.Normalize();
+		direction *= speed;
+
+		transform.Translate(direction);
+	}
+}
